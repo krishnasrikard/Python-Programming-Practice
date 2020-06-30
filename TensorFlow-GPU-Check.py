99c8e7e360269bb50762a141d58788e63657de8d
Krishna Srikar Durbha <srikarkrishnad@gmail.com>
2020-07-01 04:07:18 +0530

    Solutions in case of Errors

--- TensorFlow-GPU-Check.py
+++ TensorFlow-GPU-Check.py
@@ -1,5 +1,27 @@
 import tensorflow as tf
 
+'''
+# Refer in case of Error: failed to create cublas handle: CUBLAS_STATUS_NOT_INITIALIZED
+
+https://github.com/tensorflow/tensorflow/issues/9489#issuecomment-616949644
+
+or
+
+If getting an Error of no-package found libcudart.so.10.1, run the following Commands after installing Cuda-10.1
+
+```
+dpkg -l | grep libcublas
+```
+If version is 10.2.2.89, then runtime
+```
+sudo apt remove libcublas10
+sudo apt install libcublas10=10.2.1.243-1
+```
+
+'''
+
+
+
 with tf.device('/gpu:0'):
 	a = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
 	b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
